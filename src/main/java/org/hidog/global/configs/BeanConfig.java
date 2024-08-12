29d4a63adda7a36733f751685df953fe1d75c887
jeongsuri <jeongsuri96@gmail.com>
2024-08-12 11:51:07 +0900

    feat: 파일 기능 업로드
    프론트에서 구현한 파일기능 업로드

--- src/main/java/org/hidog/global/configs/BeanConfig.java
+++ src/main/java/org/hidog/global/configs/BeanConfig.java
@@ -1,8 +1,35 @@
 package org.hidog.global.configs;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@RequiredArgsConstructor
 public class BeanConfig {
-    
+    //자주 사용되는 의존성
+    private final EntityManager em;
+
+    @Bean
+    public JPAQueryFactory queryFactory() {
+        return new JPAQueryFactory(em);
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper om = new ObjectMapper();
+        om.registerModule(new JavaTimeModule());
+
+        return om;
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
