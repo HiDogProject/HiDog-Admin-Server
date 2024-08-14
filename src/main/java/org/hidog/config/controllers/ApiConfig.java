9ebbaeca55c5fb4f775f0cf07e0ce3a8323c588e
jeongsuri <jeongsuri96@gmail.com>
2024-08-14 17:59:52 +0900

    feat: API설정 결제설정 뷰 변경
    API설정 결제설정 뷰 변경

--- src/main/java/org/hidog/config/controllers/ApiConfig.java
+++ src/main/java/org/hidog/config/controllers/ApiConfig.java
@@ -4,10 +4,6 @@ import lombok.Data;
 
 @Data
 public class ApiConfig {
-    private String publicOpenApiKey; // 공공 API 인증키
-
-    private String kakaoJavascriptKey; // 카카오 API - 자바스크립트 앱 키
     private String tmapJavascriptKey; //Tmap API 자바스크립트 앱 키
-
     private String checkoutApiKey; // 이니시스 결제 키
 }
