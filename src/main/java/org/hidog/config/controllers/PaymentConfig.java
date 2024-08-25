f02dbc35b057a9a76cfb3241dfd6ad183df41a60
jeongsuri <jeongsuri96@gmail.com>
2024-08-25 19:06:48 +0900

    feat : 결제 api 설정

--- src/main/java/org/hidog/config/controllers/PaymentConfig.java
+++ src/main/java/org/hidog/config/controllers/PaymentConfig.java
@@ -9,5 +9,7 @@ public class PaymentConfig {
 
     private String mid; //상점 아이디 (이니시스)
     private String signKey; // 사인키
+    private String iniApiKey; // 취소시 필요
+    private String iniApiIv; //취소시 필요
     private List<String> payMethods; //결제 수단
 }
