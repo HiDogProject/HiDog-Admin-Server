167d6f319cf9ce2a0ecd34e9c93b46f4e89f5338
jeongsuri <jeongsuri96@gmail.com>
2024-08-26 17:20:23 +0900

    feat : FRONT서버에서 BoardData GET

---
+++ src/main/java/org/hidog/board/services/BoardUpdateService.java
@@ -0,0 +1,53 @@
+package org.hidog.board.services;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.hidog.board.controllers.RequestBoard;
+import org.hidog.board.entities.BoardData;
+import org.hidog.global.Utils;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class BoardUpdateService {
+    private final RestTemplate restTemplate;
+    private final ObjectMapper om;
+    private final Utils utils;
+
+    public List<BoardData> update(String mode, List<BoardData> items){
+        mode = StringUtils.hasText(mode) ? mode : "update";
+        String url = utils.url("/board/admin" + mode, "front-service");
+        Map<String, List<BoardData>> params = new HashMap<>();
+        params.put("items", items);
+
+        try {
+            String jsonBody = om.writeValueAsString(params);
+            HttpHeaders headers = utils.getCommonHeaders("POST");
+            HttpEntity<String> request = new HttpEntity<>(jsonBody, headers);
+
+            ResponseEntity<String> response = restTemplate.exchange(URI.create(url), HttpMethod.POST, request, String.class);
+            System.out.println(response);
+
+        }catch(JsonProcessingException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public BoardData update(String mode, RequestBoard form){
+        mode = StringUtils.hasText(mode) ? mode : mode;
+        return null;
+    }
+}
